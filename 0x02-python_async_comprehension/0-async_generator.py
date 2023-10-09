88aa014fc9aa85c95b9a7085b15cac07f0d7d490
JohnsonGith <johnsonwebsax@gmail.com>
2023-10-09 22:16:04 +0100

    Async commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python3
-'''Task 0's module.
-'''
 import asyncio
 import random
-from typing import Generator
 
+async def wait_random(max_delay=10):
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
 
-async def async_generator() -> Generator[float, None, None]:
-    '''Generates a sequence of 10 numbers.
-    '''
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.random() * 10
+# Example usage:
+async def main():
+    random_delay = await wait_random()
+    print(f"Random delay: {random_delay} seconds")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
