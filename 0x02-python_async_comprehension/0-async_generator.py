936afe399d3e33fe85adea7caa02234ef856358e
JohnsonGith <johnsonwebsax@gmail.com>
2023-10-09 22:18:27 +0100

    Async commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,15 +1,21 @@
+#!/usr/bin/env python3
+"""
+Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds.
+"""
+
 import asyncio
 import random
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: float = 10) -> float:
+    """
+    Wait for a random delay between 0 and max_delay seconds.
+
+    Args:
+        max_delay (float, optional): The maximum delay in seconds (default is 10).
+
+    Returns:
+        float: The random delay in seconds.
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
-# Example usage:
-async def main():
-    random_delay = await wait_random()
-    print(f"Random delay: {random_delay} seconds")
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
